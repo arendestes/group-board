87754ceea383f870515db444abf739e8a6265917
arendEstes <arendestes@yahoo.com>
2020-01-20 20:55:13 -0600

    Added templates for dashboard, project list, and notifications/ used switch and rout to add components into app

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+const ProjectList = () =>{
+
+    return(
+        <div className="project-list section">
+
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posed by Arend</p>
+                    <p className="grey-text">Jan 20, 2020</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posed by Arend</p>
+                    <p className="grey-text">Jan 20, 2020</p>
+                </div>
+            </div>
+            
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posed by Arend</p>
+                    <p className="grey-text">Jan 20, 2020</p>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default ProjectList;
