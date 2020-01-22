8ad9d0231933062d4c2e5381564127cf050c4831
arendEstes <arendestes@yahoo.com>
2020-01-22 16:00:04 -0600

    created template in project summary to import into project list in dash

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 
 const ProjectList = () =>{
@@ -6,31 +7,9 @@ const ProjectList = () =>{
     return(
         <div className="project-list section">
 
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posed by Arend</p>
-                    <p className="grey-text">Jan 20, 2020</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posed by Arend</p>
-                    <p className="grey-text">Jan 20, 2020</p>
-                </div>
-            </div>
-            
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posed by Arend</p>
-                    <p className="grey-text">Jan 20, 2020</p>
-                </div>
-            </div>
-            
+        <ProjectSummary />
+           
+        
         </div>
     )
 }
