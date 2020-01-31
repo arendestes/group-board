040e76b66b061d150eceda805c6c8ebc2f53641f
Arend Estes <42525228+arendestes@users.noreply.github.com>
2020-01-31 16:08:36 -0600

    set up birebase config

---
+++ src/config/fbConfig.js
@@ -0,0 +1,21 @@
+  import firebase from 'firebase/app';
+  import 'firebase/firestore';
+  import 'firebase/auth';
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCV4d6viVpsZaQhF446NXSP5wzC7LTWu6E",
+    authDomain: "group-board-f813b.firebaseapp.com",
+    databaseURL: "https://group-board-f813b.firebaseio.com",
+    projectId: "group-board-f813b",
+    storageBucket: "group-board-f813b.appspot.com",
+    messagingSenderId: "281537906035",
+    appId: "1:281537906035:web:1b15dc57e237ee3ba07106",
+    measurementId: "G-1BPG15EMTW"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  firebase.firestore().settings({timestampsInSnapshots: true});
+
+  export default firebase;
